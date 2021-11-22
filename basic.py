207a285b20bbf4e14d8b059ad83548bbf7b76d7a
David Han <david0han@berkeley.edu>
2021-11-22 09:02:35 -0800

    finished up to 5

--- basic.py
+++ basic.py
@@ -42,7 +42,33 @@ def largestprimefactor(num):
 
 print("problem 3: ", largestprimefactor(600851475143))
 
-def largestpalindromeproduct():
-    print()
+def largestpalindromeproduct(num):
+    biggest = 1
+    while num > 0:
+        for i in range(num, 0, -1):
+            curr = i * num
+            if str(curr) == str(curr)[::-1]:
+                if curr > biggest:
+                    biggest = curr
+        num -= 1
 
-print("problem 4: ", largestpalindromeproduct())
+    return biggest
+
+print("problem 4: ", largestpalindromeproduct(999))
+
+def smallestmultiple(num):
+    result = 1
+    lst = [x for x in range(1, num + 1)]
+    while lst:
+        print(lst)
+        curr = lst[0]
+        result *= curr
+        for i in range(len(lst)):
+            if lst[i] % curr == 0:
+                lst[i] = lst[i] // curr
+        lst = [x for x in lst if x > 1]
+
+    return result
+
+print("problem 5: ", smallestmultiple(20))
+
